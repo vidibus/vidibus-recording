e22f61120fc1f1ae4cc683f57f0f72c23e87572c
punkrats <andre@webwarelab.com>
2013-07-30 14:16:40 +0200

    Add monitoring loop

--- lib/vidibus/recording.rb
+++ lib/vidibus/recording.rb
@@ -4,3 +4,61 @@ require 'vidibus/recording/helpers'
 require 'vidibus/recording/part'
 require 'vidibus/recording/mongoid'
 require 'vidibus/recording/railtie' if defined?(Rails::Railtie)
+
+module Vidibus
+  module Recording
+    extend self
+
+    class Error < StandardError; end
+
+    INTERVAL = 1
+
+    attr_accessor :logger, :autoload_paths, :classes, :monitoring_interval
+    @logger = Logger.new(STDOUT)
+    @autoload_paths = []
+    @classes = []
+    @monitoring_interval = 1
+
+    # Monitor all started recordings
+    def monitor
+      autoload
+      unless classes.any?
+        logger.error("[#{Time.now.utc}] - No recording classes given")
+      else
+        logger.info("[#{Time.now.utc}] - Watching recordings")
+        run
+      end
+    end
+
+    # Obtain all classes that include the Mongoid module
+    def autoload
+      return [] unless autoload_paths.any?
+      regexp = /class ([^<\n]+).+include Vidibus::Recording::Mongoid/m
+      names = Dir[*autoload_paths].map do |f|
+        File.read(f)[regexp, 1]
+      end.compact
+      self.classes = names.map { |k| k.constantize }
+    end
+
+    private
+
+    def run
+      loop do
+        classes.each do |klass|
+          klass.started.each do |recording|
+            begin
+              if recording.worker_running?
+                recording.track_progress
+              else
+                recording.resume
+              end
+            rescue => e
+              logger.error("[#{Time.now.utc}] - ERROR in Vidibus::Recorder.monitor:\n#{e.inspect}\n---\n#{e.backtrace.join("\n")}")
+            end
+          end
+        end
+        sleep(monitoring_interval)
+      end
+    end
+  end
+end
---
+++ spec/vidibus/recording_spec.rb
@@ -0,0 +1,128 @@
+require 'spec_helper'
+
+describe Vidibus::Recording do
+  let(:this) { Vidibus::Recording }
+
+  def stub_loop(sleep = false)
+    if sleep == false
+      stub(this).sleep
+    end
+    stub(this).loop do |block|
+      block.call
+    end
+  end
+
+  describe '.monitor' do
+    let(:recording) do
+      Recording.create({
+        :name => 'N-TV Live',
+        :stream => 'rtmp://fms.rtl.de/ntvlive/livestream/channel1'
+      })
+    end
+
+    it 'should call #autoload' do
+      stub(this).run
+      mock(this).autoload { [] }
+      this.monitor
+    end
+
+    it 'should not run without recording classes' do
+      dont_allow(this).run
+      this.monitor
+    end
+
+    context 'with recording classes available' do
+      before do
+        this.autoload_paths = ['app/models/*.rb']
+      end
+
+      it 'should call #run' do
+        mock(this).run
+        this.monitor
+      end
+
+      it 'should loop endlessly' do
+        mock(this).loop
+        this.monitor
+      end
+
+      it 'should sleep after each iteration' do
+        stub_loop(true)
+        mock(this).sleep(Vidibus::Recording.monitoring_interval)
+        this.monitor
+      end
+
+      it 'should do nothing without started recordings' do
+        stub_loop
+        dont_allow.any_instance_of(Recording).resume
+        this.monitor
+      end
+
+      context 'with started recordings' do
+        before do
+          stub(recording).start_worker
+          recording.start
+          stub(Recording).started { [recording] }
+        end
+
+        it 'should log exceptions' do
+          stub_loop
+          stub.any_instance_of(Recording).worker_running? do
+            raise 'That went wrong'
+          end
+          mock(this.logger).error.with_any_args
+          expect { this.monitor }.not_to raise_error
+        end
+
+        context 'with a running worker' do
+          before do
+            stub.any_instance_of(Recording).worker_running? { true }
+          end
+
+          it 'should track progress' do
+            stub_loop
+            mock.any_instance_of(Recording).track_progress
+            this.monitor
+          end
+
+          it 'should not resume' do
+            stub_loop
+            dont_allow.any_instance_of(Recording).resume
+            this.monitor
+          end
+        end
+
+        context 'without a running worker' do
+          before do
+            stub.any_instance_of(Recording).worker_running? { false }
+          end
+
+          it 'should resume' do
+            stub_loop
+            mock.any_instance_of(Recording).resume
+            this.monitor
+          end
+        end
+      end
+    end
+  end
+
+  describe '.autoload' do
+    it 'should do nothing unless autoload paths have been defined' do
+      this.autoload_paths = []
+      dont_allow(Dir)[]
+      this.autoload
+    end
+
+    it 'should return all recording classes in autoload paths' do
+      this.autoload_paths = ['app/models/*.rb']
+      this.autoload.should eq([Recording])
+    end
+
+    it 'should set classes variable' do
+      this.autoload_paths = ['app/models/*.rb']
+      this.autoload
+      this.classes.should eq([Recording])
+    end
+  end
+end
