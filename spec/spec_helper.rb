2ab4fa7187f926b925691bca421b67691f0dd325
punkrats <andre@webwarelab.com>
2013-07-30 14:11:44 +0200

    Remove old time stub

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,17 +25,3 @@ RSpec.configure do |config|
     Mongoid.master.collections.select {|c| c.name !~ /system/}.each(&:drop)
   end
 end
-
-# Helper for stubbing time. Define String to be set as Time.now.
-# Usage:
-#   stub_time('01.01.2010 14:00')
-#   stub_time(2.days.ago)
-#
-def stub_time(string = nil)
-  string ||= Time.now.to_s(:db)
-  now = Time.parse(string.to_s)
-  stub(Time).now { now }
-  now
-end
-
-#I18n.load_path += Dir[File.join('config', 'locales', '**', '*.{rb,yml}')]
