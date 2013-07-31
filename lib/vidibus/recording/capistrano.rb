6b2c64c21bb617d0084951b8d23b02f6e0cffb94
punkrats <andre@webwarelab.com>
2013-07-31 20:54:41 +0200

    Add Capistrano integration of monitoring daemon

---
+++ lib/vidibus/recording/capistrano.rb
@@ -0,0 +1,12 @@
+require 'vidibus/recording/capistrano/recipes'
+
+# Run Capistrano Recipes for monitoring recordings.
+#
+# Load this file from your Capistrano config.rb:
+# require 'vidibus/recording/capistrano'
+#
+Capistrano::Configuration.instance.load do
+  after 'deploy:stop',    'vidibus:recording:stop'
+  after 'deploy:start',   'vidibus:recording:start'
+  after 'deploy:restart', 'vidibus:recording:restart'
+end
---
+++ lib/vidibus/recording/capistrano/recipes.rb
@@ -0,0 +1,40 @@
+# Capistrano Recipes for monitoring recordings.
+#
+# Load this file from your Capistrano config.rb:
+# require 'vidibus/recording/capistrano/recipes'
+#
+# Add these callbacks to have the recording process restart when the server
+# is restarted:
+#
+#   after 'deploy:stop',    'vidibus:recording:stop'
+#   after 'deploy:start',   'vidibus:recording:start'
+#   after 'deploy:restart', 'vidibus:recording:restart'
+#
+Capistrano::Configuration.instance.load do
+  namespace :vidibus do
+    namespace :recording do
+      def rails_env
+        fetch(:rails_env, false) ? "RAILS_ENV=#{fetch(:rails_env)}" : ''
+      end
+
+      def roles
+        fetch(:app)
+      end
+
+      desc 'Stop the recording process'
+      task :stop, :roles => lambda { roles } do
+        run "cd #{current_path};#{rails_env} script/recording stop"
+      end
+
+      desc 'Start the recording process'
+      task :start, :roles => lambda { roles } do
+        run "cd #{current_path};#{rails_env} script/recording start"
+      end
+
+      desc 'Restart the recording process'
+      task :restart, :roles => lambda { roles } do
+        run "cd #{current_path};#{rails_env} script/recording restart"
+      end
+    end
+  end
+end
