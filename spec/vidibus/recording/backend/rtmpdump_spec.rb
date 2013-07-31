79ea6ab5e8e3d979766b181f8174edd48f543598
punkrats <andre@webwarelab.com>
2013-07-31 15:00:00 +0200

    Improve and extend specs for rtmpdump 2.4

--- spec/vidibus/recording/backend/rtmpdump_spec.rb
+++ spec/vidibus/recording/backend/rtmpdump_spec.rb
@@ -14,8 +14,9 @@ describe 'Vidibus::Recording::Backend::Rtmpdump' do
   end
   let(:success_v22) { read_log('success_v22') }
   let(:success_v23) { read_log('success_v23') }
+  let(:success_v24) { read_log('success_v24') }
 
-  describe 'extract_metadata' do
+  describe '#extract_metadata' do
     it 'should extract relevant metadata from RTMPDump v2.2' do
       this.extract_metadata(success_v22).should eql({
         'presetname' => 'Custom',
@@ -55,6 +56,29 @@ describe 'Vidibus::Recording::Backend::Rtmpdump' do
         'audioinputvolume' => '75.00',
         'audiocodecid' => '.mp3',
         'audiodatarate' => '48.00'
+      })
+    end
+
+    it 'should extract relevant metadata from RTMPDump v2.4' do
+      this.extract_metadata(success_v24).should eql({
+        'audiocodecid' => '10.00',
+        'audiodatarate' => '124.89',
+        'audiosamplerate' => '48000.00',
+        'audiosamplesize' => '16.00',
+        'comment' => 'www.dvdvideosoft.com',
+        'compatible_brands' => 'mp42isomavc1',
+        'creation_time' => '2013-02-26 13:47:56',
+        'date' => '2013',
+        'duration' => '0.00',
+        'encoder' => 'Lavf53.8.0',
+        'filesize' => '0.00',
+        'framerate' => '25.00',
+        'height' => '350.00',
+        'major_brand' => 'mp42',
+        'stereo' => 'TRUE',
+        'videocodecid' => '7.00',
+        'videodatarate' => '976.57',
+        'width' => '620.00'
       })
     end
   end
